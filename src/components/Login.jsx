3999ce20cb098f79c760c2906b28bd8be605045d
Eddahkarambu <gikundaeddah@gmail.com>
2023-05-02 13:40:00 +0300

    install tailwind css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import loginimg  from "../assets/login.jpg";
 const Login = () =>{
     return(
-        <div className="grid, grid-cols-1 sm:grid-cols-2 h-screen w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 bg-black  h-screen w-full">
             <div className="hidden sm:block">
             <img className="w-full h-full object-cover" src={loginimg} alt=""/>
             </div>
 
-            <div className="bg-black"> 
+            <div className="bg-gray-400 flex flex-col justify-center"> 
                 <form>
                      <h1>SIGN IN</h1>
                      <div>
